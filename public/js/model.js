01107612f1c22b0a359c8c07e9b9ee8122635472
felix <wangfelix87@gmail.com>
2017-08-29 09:53:50 +0800

    fix error when stream is empty

--- public/js/model.js
+++ public/js/model.js
@@ -59,7 +59,13 @@ function setLiveStream(data)
         var bytes_out = "<td class='text-center'>"+byteToHuman(channel.bytes_out[0])+"</td>";
         var time  = "<td class='text-center'>"+secondsToHuman(channel.time[0] / 1000)+"</td>";
         var viewers = "<td class='text-center'><b>"+numeral(channel.nclients[0]).format('0,0')+"<b/></td>";
-        var resolution = "<td class='text-center'>"+channel.meta[0].video[0].width[0] + " X " + channel.meta[0].video[0].height[0]+"</td>";
+	var v_w = 0;
+	var v_h = 0;
+	if (typeof channel.meta !== 'undefined') {
+	    v_w = chanel.meta[0].video[0].width[0];
+	    v_h = channel.meta[0].video[0].height[0];
+	}
+        var resolution = "<td class='text-center'>"+ v_w + " X " + v_h+"</td>";
         var play = "<td><button class='btn btn-default play_stream' data-stream-name='"+channel.name[0]+"' data-toggle='modal' data-target='#stream_popup'><i class='glyphicon glyphicon-play'></i> "+language_play+"</button></td>";
 	    var drop = "<td><button class='btn btn-default drop_stream' data-stream-name='"+channel.name[0]+"'><i class='glyphicon glyphicon-stop'></i> "+language_drop+"</button></td>";
         table = table + "<tr>"+name+resolution+bw_in+bw_out+bytes_in+bytes_out+viewers+time+play+drop+"</tr>";
