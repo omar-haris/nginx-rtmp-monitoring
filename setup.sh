a0b3633523d7362fe843a77556d57d8b9a4b9c37
Omar Khalid <fiftysoft@gmail.com>
2021-01-19 16:25:52 +0300

    generate random key for session

--- setup.sh
+++ setup.sh
@@ -15,11 +15,15 @@ function replace_in_file() {
 #find host ip address
 ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
+#generate session key
+key=$(openssl rand -hex 32)
+
 #configure the dashboard
 $(replace_in_file 's,rtmp_server_url.*,rtmp_server_url\":'"\"http://$ip:8080/stat.xml"\"'\,,' "config.json")
 $(replace_in_file 's,rtmp_server_stream_url.*,rtmp_server_stream_url\":'"\"rtmp://$ip:1935/live/"\"'\,,' "config.json")
 $(replace_in_file 's,rtmp_server_control_url.*,rtmp_server_control_url\":'"\"http://$ip:8080/control"\"'\,,' "config.json")
 $(replace_in_file 's,hls_server_stream_url.*,hls_server_stream_url\":'"\"http://$ip:8080/live/"\"'\,,' "config.json")
+$(replace_in_file 's,session_secret_key.*,session_secret_key\":'"\"$key"\"'\,,' "config.json")
 
 #start the contrainers
 docker-compose up --build -d
